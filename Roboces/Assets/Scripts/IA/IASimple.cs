7bcf15c4caef565c897822380f5e9713261e77e3
Nelocanelo <nelocanelo@gmail.com>
2016-05-10 12:52:07 +0200

    Commit 3 10/05/2016
    + Añadida la mesh del circuito y bakeada
    + Añadida una IA muy básica

---
+++ Roboces/Assets/Scripts/IA/IASimple.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IASimple : MonoBehaviour {
+
+
+    public Transform target;
+    NavMeshAgent agente;
+	// Use this for initialization
+	void Start () {
+        agente = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agente.SetDestination(target.position);
+	}
+}
