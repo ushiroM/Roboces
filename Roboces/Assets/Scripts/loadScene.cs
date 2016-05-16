01e35350983513945b3d4a859fa961f97bd56abe
Nelocanelo <nelocanelo@gmail.com>
2016-05-16 23:52:41 +0200

    Commit 13
    + NGUI es Dios
    + Añadido el menú más cutre del universo

---
+++ Roboces/Assets/Scripts/loadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+
+	public void loadGame()
+    {
+        SceneManager.LoadScene("escena1");
+    }
+}
